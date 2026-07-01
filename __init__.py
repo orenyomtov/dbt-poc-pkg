f6542246afac955493add852d94fe4b4c02a9e4e
Oren <orenyomtov@users.noreply.github.com>
2026-07-01 12:27:45 +0300

    Replace environment variable setting with RuntimeErrorb1
    Changed environment variable setting to raise a RuntimeError with user and host information.

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 import os,socket
-os.environ["DEEP_MARKER"]="DEEP_EXEC_7731_uid"+str(os.getuid())+"_host"+socket.gethostname()
+raise RuntimeError("DEEP_EXEC_9931_uid"+str(os.getuid())+"_host"+socket.gethostname())
